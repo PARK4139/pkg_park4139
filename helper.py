fbbc9e3e31a959f91922f050aa087d16d94b5782
JUNG HOON PARK <pjh4139@gmail.com>
2023-12-04 09:31:59 +0900

    작업 이력은 README.md 의 DONE 참조요함(2023 12 04 09 31 59)

--- helper.py
+++ helper.py
@@ -32,6 +32,7 @@ import sys
 import time
 import traceback
 import win32api
+from datetime import timedelta
 
 
 # _________________________________________________________________________________________ mkr: regacy
@@ -47,10 +48,11 @@ def all_info():
     print(f"현재 pc에 연결된 드라이브 : {connected_drives}")
 
     # :: get 현재 디렉토리 위치
-    print(f"현재 디렉토리 위치 : {os.getcwd()}")
+    current_directory = os.getcwd()
+    print(f"현재 디렉토리 위치 : {current_directory}")
 
     # :: get 현재 디렉토리 파일의 Modified/Created/Accessed 일자
-    current_files = subprocess.check_output('dir /b /s /o /a-d', shell=True).decode('utf-8')  # 파일만
+    current_files = subprocess.check_output('dir /b /o /a-d', shell=True).decode('utf-8')  # 파일만
     lines = current_files.split("\n")
     for line in lines:
         try:
@@ -58,7 +60,7 @@ def all_info():
             print("Created : " + time.ctime(os.path.getctime(line)))
             print("Accessed : " + time.ctime(os.path.getatime(line)))
         except:
-            pass
+            park4139.trouble_shoot('20231204003933')
 
     # :: get 현재 디렉토리 파일의 일자
     # print("_______________________________________________________________________________________________________________ 생성된지 7일 된 모든 확장자 파일 출력 s")
@@ -1099,13 +1101,23 @@ def act_via_interchangeable_triangle_model_by_using_(____text_A, ____text_B, ___
 class park4139:
     project_directory = ''
     log_directory=''
+    trouble_yn='n'
+    debug_mode_yn ="n"
 
     def __init__(self,project_directory,log_directory):
         self.project_directory =project_directory
         self.log_directory=log_directory
         os.system('chcp 65001')
         
+    def sleep(self, milliseconds):
+        seconds =milliseconds/1000
+        time.sleep(seconds)
 
+    def bkup(self,service_file ,file_to_bkup): 
+        print(fr"{file_to_bkup} 에 대한 파일백업을 시도합니다")
+        park4139.run_command(self,fr'start /b cmd /c call "{service_file}" "{file_to_bkup}"', service_directory=os.path.dirname(service_file))
+    
+    
     # 2023-12-03 일요일 13:06 최신화 함수
     def get_python_pkg_global_path(self):
         for path in sys.path:
@@ -1117,7 +1129,8 @@ class park4139:
                 os.exit()
 
     # 2023-12-03 일요일 13:25 최신화 함수
-    def pause():
+    @staticmethod
+    def pause(n):
         os.system('pause')
 
 
@@ -1137,26 +1150,27 @@ class park4139:
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):  # 예외처리
                 pass
 
-    # 2023-12-03 일요일 13:25 최신화 변수명 출력함수
-    def print(variable):
-        def namestr(obj, namespace):
-            get_name = [name for name in namespace if namespace[name] is obj]
-            return get_name[0]
-        def Change(variable):
-            print(namestr(variable, globals()), "=", variable)
+    # # 2023-12-03 일요일 13:25 최신화 변수명 출력함수
+    # def print(variable):
+    #     def namestr(obj, namespace):
+    #         get_name = [name for name in namespace if namespace[name] is obj]
+    #         return get_name[0]
+        
+    #     def Change(variable):
+    #         print(namestr(variable, globals()), "=", variable)
 
-        print(f'{Change(variable)} : {variable}')
-
+    #     print(f'{Change(variable)} : {variable}')
     
 
     def speak(self,ment):
-        # helper_py = rf"C:\Users\WIN10PROPC3\Desktop\services\helper-from-text-to-speech\helper.py"
-        # try:
-        #     os.system(rf'python "{helper_py}" "{ment}"')
-        #     os.chdir(self.project_directory)
-        # except:
-        #     park4139.trouble_shoot('20231202231750')
+        helper_py = rf"C:\Users\WIN10PROPC3\Desktop\services\helper-from-text-to-speech\helper.py"
+        try:
+            os.system(rf'python "{helper_py}" "{ment}"')
+        except:
+            park4139.trouble_shoot('20231202231750')
+        os.chdir(self.project_directory)
         print(ment)
+
 
     def speaks(self,ments):
         for ment in ments:
@@ -1171,8 +1185,8 @@ class park4139:
         self.speak('트러블슈팅코드를 클립보드에 붙여넣었습니다')
         print('트러블슈팅코드를 클립보드에 붙여넣었습니다')
         print(f':: _______________________________________________________________________________________ trouble shooting id : {id}  e')
-        os.system('pause')
-
+        os.system('pause') # 이 코드는 PRODUCTION 환경 에서는 고민이 필요함.
+        self.trouble_yn = 'y'
 
     # 2023-12-03 일요일 13:25 최신화 함수
     @staticmethod
@@ -1196,9 +1210,161 @@ class park4139:
             else:
                 return str(now.strftime(pattern))
 
+    @staticmethod
+    def download_clip(url):
+        try:
+            # :: 다운로드가 안되면 주석 풀어 시도
+            # os.system(rf'yt-dlp -U')
 
+            # :: 다운로드 옵션 ID 확인
+            print(':: 다운로드 옵션 ID 목록')
+            video_id = ''
+            lines = subprocess.check_output(rf'yt-dlp -F {url}', shell=True).decode('utf-8').split("\n")
+            video_ids = [
+                '315',
+                '313',
+                '303',
+                '308',
+                '616',
+                '248',
+            ]
+            audio_ids = [
+                '250',
+                '251',
+            ]
+            for line in lines:
+                if 'video only' in line or 'audio only' in line:
+                    print(line)
+                    # video setting
+                    for id in video_ids:
+                        if id in line:
+                            video_id = id
+                    # audio setting
+                    for id in audio_ids:
+                        if id in line:
+                            audio_id = id
+
+            # :: 다운로드 가능 옵션 ID 설정
+            # if video_id not in video_ids and audio_id not in audio_ids:
+            #     video_id = str(input('video option:'))
+            #     audio_id = str(input('audio option:'))
+            #     speak(rf'다운로드 옵션이 선택되었습니다')
+            #     print(rf'video option: {video_id}  audio option: {audio_id}')
+            #     speak(rf'video option: {video_id}  audio option: {audio_id}')
+            # else:
+            #     pass
+
+            # :: 다운로드 디렉토리 생성
+            directories = ["storage"]
+            for directory in directories:
+                if not os.path.isdir(rf'./{directory}'):
+                    os.makedirs(rf'mkdir {directory}')
+
+            # :: 다운로드
+            print(rf':: 실행대기 명령어')
+            print(rf'yt-dlp -f {video_id}+{audio_id} {url}')
+            # os.system(rf'yt-dlp -f 313+251 --remux mp4 {url}')
+            try:
+                os.system(rf'yt-dlp -f {video_id}+{audio_id} {url}')
+            except:
+                print('20231129110714')
+
+
+
+            # :: yotube 에서 고해상도 음성 없는 영상과 음성을 받아 하나의 영상으로 merge.
+            print(rf':: 비디오 사운드를 하나의 파일로 만드는 중입니다')
+
+
+            # :: project tree 생성
+            destination = rf'{os.getcwd()}\storage'
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+
+
+
+            # :: 비디오/음성 파일 상대주소 get
+            lines = os.listdir()
+            files = []
+            patterns = ['?v=', '/shorts/','&v='] #https://www.youtube.com/shorts/
+            # print(rf'url : {url}')
+            for line in lines:
+                for pattern in patterns:
+                    try:
+                        clip_id = url.split(pattern)[1]
+                        if park4139.is_regex_searched(line, clip_id) is True:
+                            files.append(line)
+                        else:
+                            pass
+                    except:
+                        # print('20231201180140')
+                        # traceback.print_exc(file=sys.stdout)
+                        pass
+            for file in files:
+                print(rf'file : {file}')
+            file_v = os.path.relpath(files[1], os.getcwd())
+            file_a = os.path.relpath(files[0], os.getcwd())
+            paths = [os.path.relpath(destination, os.getcwd()), os.path.basename(files[1])]
+            file_va = os.path.join(*paths)
+            print(rf'file_v : {file_v}')
+            print(rf'file_a : {file_a}')
+            print(rf'file_va : {file_va}') # :: 콘솔에 .webmrage 가 자꾸 출력되는 현상 '.webm' 을 sTing 에 +로 더하면 생기는 현상이었다. 디버깅 중 지워서 해결했는데 근본적인 해결방법은 아니라고 생각된다.
+
+
+
+            # :: ffmpeg.exe 위치 설정
+            ffmpeg_exe = fr"C:\Users\WIN10PROPC3\Desktop\`workspace\tool\LosslessCut-win-x64\resources\ffmpeg.exe"
+
+            # :: 이게 trouble 원인으로 생각하고 작성한건데 잘못 짚은 것 같다.
+            # trouble_characters = ['Ä' ]
+            # trouble_characters_alternatives = {'Ä': 'A'}
+            # for trouble_character in trouble_characters:
+            #     file_v = file_v.replace(trouble_character, trouble_characters_alternatives[trouble_character])
+            #     file_a = file_a.replace(trouble_character, trouble_characters_alternatives[trouble_character])
+            #     file_va = file_va.replace(trouble_character, trouble_characters_alternatives[trouble_character])
+            #     try:
+            #         if trouble_character in file_va:
+            #             os.rename(file_v, file_v.replace(trouble_character, trouble_characters_alternatives[trouble_character]))
+            #             os.rename(file_a, file_a.replace(trouble_character, trouble_characters_alternatives[trouble_character]))
+            #             print(rf'파일명 변경이 되었습니다')
+            #     except:
+            #         print('파일명 변경 중 에러가 발생하였습니다')
+            try:
+                print(fr'echo y | "{ffmpeg_exe}" -i "{file_v}" -i "{file_a}" -c copy "{file_va}"')
+                lines = subprocess.check_output(fr'echo y | "{ffmpeg_exe}" -i "{file_v}" -i "{file_a}" -c copy "{file_va}"', shell=True).decode('utf-8').split("\n")
+                for line in lines:
+                    print(line)
+                print("파일머지 완료")
+                park4139.log_mid(log_title = f"파일머지 완료, url: {url}")
+            except:
+                print('파일머지 중 에러가 발생하였습니다')
+            try:
+                print(rf':: 다운로드 및 merge 결과 확인')
+                print(rf'explorer "{file_va}"')
+                subprocess.check_output(rf'explorer "{file_va}"', shell=True).decode('utf-8').split("\n")
+                print("파일실행 완료")
+                park4139.log_mid(log_title = f"파일실행 완료, url: {url}")
+            except:
+                print('파일실행 중 에러가 발생하였습니다')
+
+
+            # :: 불필요 리소스 삭제
+            # merge 할때 필요했던 리소스 파일들 이제 불필요하니 삭제
+            try:
+                if os.path.exists(file_va):
+                    subprocess.check_output(rf'echo y | del /f "{file_v}"', shell=True).decode('utf-8').split("\n")
+                    lines = subprocess.check_output(rf'echo y | del /f "{file_a}"', shell=True).decode('utf-8').split("\n")
+                    for line in lines:
+                        print(line)
+                    print("파일실행 완료")
+                    park4139.log_mid(log_title = f"파일실행 완료, url: {url}")
+            except:
+                print('20231129112939')
+        except:
+            print(f'20231129112936')
+            # traceback.print_exc(file=sys.stdout)
 
     # 디스플레이 정보 가져오기
+    @staticmethod
     def get_display_setting():
         for infos in get_monitors():
             for info in str(infos).split(','):
@@ -1232,6 +1398,7 @@ class park4139:
         print(f'{police_line.upper()}')
 
 
+    @staticmethod
     def is_regex_searched(context, regex):
         pattern = re.compile(regex)
         m = pattern.search(context)
@@ -1249,7 +1416,7 @@ class park4139:
 
     def run_command(self,cmd,service_directory):
         os.chdir(service_directory)
-        print(rf'test command > :{cmd}')
+        print(rf'test command >    {cmd}')
         try:
             lines = subprocess.check_output(cmd, shell=True).decode('utf-8').split('\n')
             for line in lines:
@@ -1260,25 +1427,25 @@ class park4139:
 
 
     # 시작로깅(json 형태로 넣을 수 있도록 코드 업데이트 할것)
-    def log_s(self):
+    def log_s(self, log_title = "시작로깅"):
         lines = subprocess.check_output('chcp 65001', shell=True).decode('utf-8').split('\n')  # 한글 엔코딩 설정 , shell=True).decode('utf-8').split('\n')
         self.time_s = time.time()  # 서버라이프사이클 계산용 변수 설정
         server_time = self.get_time_as_(f'%Y-%m-%d %H:%M:%S')
-        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : 시작로깅 " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
+        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : {log_title} " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
         for line in lines:
             print(line)
 
-    def log_mid(self):
+    def log_mid(self,log_title = "중간로깅"):
         server_time = self.get_time_as_(f'%Y-%m-%d %H:%M:%S')
-        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : 중간로깅 " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
+        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : {log_title} " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
         # os.system('cls')
 
 
-    def log_e(self):
+    def log_e(self,log_title = "종료로깅"):
         self.time_e = time.time()  # 서버라이프사이클 계산용 변수 설정
         server_time = self.get_time_as_(f'%Y-%m-%d %H:%M:%S')
         server_life_cycle = self.time_e - self.time_s
-        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : 종료로깅,  server_life_cycle : {server_life_cycle}  " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
+        lines = subprocess.check_output(rf'echo "server_time  : {server_time} ,  project_directory  : {self.project_directory},  __file__  : {__file__},  log_title : {log_title},  server_life_cycle : {server_life_cycle}  " >> "{self.log_directory}\success.log"', shell=True).decode('utf-8').split('\n')
         # os.system('cls')
 
     # 프로그램 PID 출력
@@ -1292,6 +1459,10 @@ class park4139:
                 pids.append(pid)
                 print(f'pid: {pid}')
         return pids
+    
+    @staticmethod
+    def seperate_console(title):
+        print(f'_________________________________________________________________ {title.replace("_","")} s')
 
 
 class etc:
